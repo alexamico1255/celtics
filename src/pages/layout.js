a1ee3afd9e092055737b23b15bac78a731df41d2
alexamico1255 <alexamico1255@gmail.com>
2024-05-29 12:06:27 -0400

    in class edits

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import { Outlet, Link } from "react-router-dom";
 import Header from "../components/header";
 import Footer from "../components/footer";
+import "../styles/nav.css";
 
 const Layout = () => {
+  
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+    console.log(menuOpen);
+  }
   return (
     <>
     <Header />
-      <nav>
-        <ul>
+    <nav id="main-nav">
+        <div id="toggle" onClick ={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+        </div>
+        <ul id="nav-items" className={menuOpen ? "" : "hide-small"}>
           <li>
             <Link to="/">Home</Link>
           </li>
