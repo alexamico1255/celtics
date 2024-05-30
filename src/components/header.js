1fc39220791f3466d7fc449bde8b8b6344f3cb35
alexamico1255 <alexamico1255@gmail.com>
2024-05-29 23:34:07 -0400

    added logo to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,40 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import '../styles/header.css';
+import '../styles/nav.css';
+import logo from "../images/logo.png";
 
 const Header = () => {
-    return(
+    const [menuOpen, setMenuOpen] = useState(false);
 
-    <header id= "main-header">
-        <h1>Celtics Central</h1>
-    </header>
-    
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+        console.log(menuOpen);
+    }
+
+    return (
+        <header id="main-header">
+                <img src={logo} id="logo" alt= "Logo" />
+            <h1>Celtics Central</h1>
+            <nav id="main-nav">
+                <div id="toggle" onClick={toggleMenu}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <ul id="nav-items" className={menuOpen ? "" : "hide-small"}>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/players">Players</Link>
+                    </li>
+                    <li>
+                        <Link to="/nextgame">Next Game</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     );
 };
 
--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,43 +1,16 @@
-import { useState } from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import "../styles/nav.css";
 
 const Layout = () => {
-  
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-    console.log(menuOpen);
-  }
-  return (
-    <>
-    <Header />
-    <nav id="main-nav">
-        <div id="toggle" onClick ={toggleMenu}>
-        <div></div>
-        <div></div>
-        <div></div>
-        </div>
-        <ul id="nav-items" className={menuOpen ? "" : "hide-small"}>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/players">Players</Link>
-          </li>
-          <li>
-            <Link to="/nextgame">nextgame</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Outlet />
-      <Footer />
-    </>
-  )
+    return (
+        <>
+            <Header />
+            <Outlet />
+            <Footer />
+        </>
+    )
 };
 
 export default Layout;
